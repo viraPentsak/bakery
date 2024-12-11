3b73540cf7cfaab8b83fb29c4f978a1483b83a46
viraPentsak <v.o.pentsak@gmail.com>
2024-12-11 18:52:20 +0200

    Updates base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {fileURLToPath, URL} from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "./",
+    base: "/bakery/",
     plugins: [
         react(),
         vitePluginSvgr()
