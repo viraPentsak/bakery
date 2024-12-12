654230718da486fdf3071adabedeedf697014b4b
viraPentsak <61148110+viraPentsak@users.noreply.github.com>
2024-12-12 18:16:13 +0200

    Fixes aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react"
 import {defineConfig} from "vite"
 import vitePluginSvgr from "vite-plugin-svgr";
+import {fileURLToPath, URL} from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
     plugins: [
         react(),
         vitePluginSvgr()
-    ]
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
 })
